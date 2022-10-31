ce847e83022352ffca743fcdd1c71c92a452cb6a
whchoi98 <whchoi98@gmail.com>
2022-10-31 17:32:47 +0900

    create yaml file for Locust

--- vpc_create.sh
+++ vpc_create.sh
@@ -2,24 +2,23 @@
 # command ./vpc_create.sh
 # ICNVPC , SINVPC, Client VPC 생성
 
-cd ~/awsga_dr
 aws cloudformation deploy \
   --region ap-northeast-2 \
   --stack-name "ICNVPC" \
-  --template-file "/home/ec2-user/environment/ICNVPC.yaml" \
+  --template-file "./ICNVPC.yaml" \
   --parameter-overrides "KeyPair=$mykey" \
   --capabilities CAPABILITY_NAMED_IAM \
   --s3-bucket ${bucket_name} &
 aws cloudformation deploy \
   --region ap-southeast-1 \
   --stack-name "SINVPC" \
-  --template-file "/home/ec2-user/environment/SINVPC.yaml" \
+  --template-file "./SINVPC.yaml" \
   --parameter-overrides "KeyPair=$mykey" \
   --capabilities CAPABILITY_NAMED_IAM &
 aws cloudformation deploy \
   --region ap-southeast-1 \
   --stack-name "Client" \
-  --template-file "/home/ec2-user/environment/Client.yaml" \
+  --template-file "./Client.yaml" \
   --parameter-overrides "KeyPair=$mykey" \
   --capabilities CAPABILITY_NAMED_IAM
   
